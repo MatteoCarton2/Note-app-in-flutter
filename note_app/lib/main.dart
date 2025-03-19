70ab7615cc44593c1c1a1d21c5971f56922ff8a1
Mattéo <mcarton@student.s19.be>
2025-03-19 19:43:28 +0100

    ex01

--- note_app/lib/main.dart
+++ note_app/lib/main.dart
@@ -5,26 +5,34 @@ void main() {
 }
 
 // MonBouton doit être ici, au même niveau que MyApp
-class MonBouton extends StatelessWidget{
+class MonBouton extends StatelessWidget {
   // Propriétés
   final String text; // "final" devant car les valeurs ne changeront pas après la création du bouton
   final Color couleur; // same
+  final double taille;
 
   // Constructeur
-  const MonBouton({super.key, required this.text, required this.couleur});
+  const MonBouton({super.key, required this.text, required this.couleur, required this.taille});
 
   // Build
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        print("Bouton cliqué !");
-      },
-      child: Text(text), // C'est comme dire : "Mon bouton a un enfant (child:)" "Qui est un texte (Text)" "Avec le contenu de ma propriété text"
-      style: ElevatedButton.styleFrom( // C'est comme dire : "Mon bouton a un style (style:)" "Créé à partir de (styleFrom)" "Avec un fond de ma couleur personnalisée" "Et un texte en blanc"
-        backgroundColor: couleur,
-        foregroundColor: Colors.white,
-      )
+    return SizedBox(
+      width: taille,
+      height: taille / 2,
+      child: ElevatedButton(
+        onPressed: () {
+          print("Bouton cliqué !");
+        },
+        child: Text(text), // C'est comme dire : "Mon bouton a un enfant (child:)" "Qui est un texte (Text)" "Avec le contenu de ma propriété text"
+        style: ElevatedButton.styleFrom( // C'est comme dire : "Mon bouton a un style (style:)" "Créé à partir de (styleFrom)" "Avec un fond de ma couleur personnalisée" "Et un texte en blanc"
+          backgroundColor: couleur,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          )
+        )
+      ),
     );
   }
 }
@@ -43,6 +51,7 @@ class MyApp extends StatelessWidget {
           child: MonBouton(
             text: "Ceci est un bouton",
             couleur: Colors.blue,
+            taille: 200,
           ),
         ),
       ),
