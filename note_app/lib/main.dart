93d37fe06c055246eb1d500d20bf5833676936dd
Mattéo <mcarton@student.s19.be>
2025-03-19 20:52:03 +0100

    ex 04 créer une liste de tâches (stateless)

--- note_app/lib/main.dart
+++ note_app/lib/main.dart
@@ -9,17 +9,17 @@ class MonBouton extends StatelessWidget {
   // Propriétés
   final String text; // "final" devant car les valeurs ne changeront pas après la création du bouton
   final Color couleur; // same
-  final double taille;
+  final double size;
 
   // Constructeur
-  const MonBouton({super.key, required this.text, required this.couleur, required this.taille});
+  const MonBouton({super.key, required this.text, required this.couleur, required this.size});
 
   // Build
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: taille,
-      height: taille / 2,
+      width: size,
+      height: size / 2,
       child: ElevatedButton(
         onPressed: () {
           print("Bouton cliqué !");
@@ -39,12 +39,12 @@ class MonBouton extends StatelessWidget {
 // ex 2 :
 class MonTexte extends StatelessWidget {
   // Propriétés
-  final double taille;
+  final double size;
   final Color couleur;
   final String text;
 
   // Constructeur
-  const MonTexte({super.key, required this.taille, required this.couleur, required this.text});
+  const MonTexte({super.key, required this.size, required this.couleur, required this.text});
 
   // Build
   @override
@@ -52,7 +52,7 @@ class MonTexte extends StatelessWidget {
     return Text(
       text,
       style: TextStyle(
-        fontSize: taille,
+        fontSize: size,
         color: couleur,
       )
     );
@@ -79,15 +79,43 @@ class CarteProfil extends StatelessWidget {
         child: Column(
         children: [
           Icon(icone),
-          MonTexte(text: nom, couleur: Colors.black, taille: 16),
-          MonTexte(text: titre, couleur: Colors.red, taille: 14),
-          MonTexte(text: description, couleur: Colors.grey, taille: 10),
+          MonTexte(text: nom, couleur: Colors.black, size: 16),
+          MonTexte(text: titre, couleur: Colors.red, size: 14),
+          MonTexte(text: description, couleur: Colors.grey, size: 10),
         ],
       ),
       ),
     );
   }
+}
 
+// ex 04
+class ListeTaches extends StatelessWidget {
+  // propriétés
+  final String text;
+  final bool done;
+  final Function(bool?) onChanged;
+  final Function() onDelete;
+
+  // constructeur
+  const ListeTaches({super.key, required this.text, required this.done, required this.onChanged, required this.onDelete});
+
+  // build
+@override
+Widget build(BuildContext context) {
+  return ListTile(
+    leading: Checkbox(
+      value: done,
+      onChanged: onChanged,
+      ),
+    title: MonTexte(text: text, couleur: Colors.black, size: 14),
+    trailing: IconButton(
+      icon: Icon(Icons.delete),
+      onPressed: onDelete,
+      color: Colors.red,
+    ),
+  );
+}
 }
 
 class MyApp extends StatelessWidget {
@@ -104,28 +132,32 @@ class MyApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,  // Pour centrer verticalement
             children: [
-              MonBouton(
-                text: "Mon bouton",
-                couleur: Colors.blue,
-                taille: 150,
-              ),
-              SizedBox(height: 20),
-              MonTexte(
-                text: "Bienvenue sur ma super application !",
-                couleur: Colors.purple,
-                taille: 18,
-              ),
-              MonTexte(
-                text: "c'est génial...",
-                couleur: Colors.black,
-                taille: 12,
-              ),
-              SizedBox(height: 20),
               CarteProfil(
                 icone: Icons.person,
                 nom: "Mattéo Carton",
                 titre: "Développeur flutter",
                 description: "En cours d'apprentissage."
+              ),
+              SizedBox(height: 32),
+              ListeTaches(
+                text: "Apprendre Flutter",
+                done: false,
+                onChanged: (bool? value) {
+                  print("La tâche 'Apprendre Flutter' est désormais : $value");
+                },
+                onDelete: () {
+                  print("La tâche 'Apprendre Flutter' a été supprimée.");
+                },
+              ),
+               ListeTaches(
+                text: "Créer ma propre application",
+                done: false,
+                onChanged: (bool? value) {
+                  print("La tâche 'Créer ma propre application' est désormais : $value");
+                },
+                onDelete: () {
+                  print("La tâche 'Créer ma propre application' a été supprimée.");
+                },
               ),
             ],
           ),
